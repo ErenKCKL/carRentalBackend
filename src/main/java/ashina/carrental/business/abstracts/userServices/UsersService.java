281541072e721531d12c72bbeace615aa6c90ba5
Eren <eren092003@gmail.com>
2023-06-19 19:10:54 +0200

    new comment block changes

--- src/main/java/ashina/carrental/business/abstracts/userServices/UsersService.java
+++ src/main/java/ashina/carrental/business/abstracts/userServices/UsersService.java
@@ -6,13 +6,28 @@ import java.util.Optional;
 import ashina.carrental.entities.concretes.users.Users;
 
 public interface UsersService {
+    // This interface defines the contract for user-related operations in the
+    // service layer
 
     List<Users> getAllUsers();
+    // Retrieves all users
+    // Returns a list of Users objects
 
     Optional<Users> findById(int id);
+    // Finds a user by their ID
+    // Takes an integer ID as a parameter
+    // Returns an Optional that may contain the user with the specified ID
 
     Optional<Users> findByEmail(String email);
+    // Finds a user by their email address
+    // Takes a string email as a parameter
+    // Returns an Optional that may contain the user with the specified email
+    // address
 
     boolean existsByEmail(String email);
+    // Checks if a user with the specified email address exists
+    // Takes a string email as a parameter
+    // Returns true if a user with the specified email address exists, false
+    // otherwise
 
 }
--- src/main/java/ashina/carrental/business/concretes/usersManagers/UsersManager.java
+++ src/main/java/ashina/carrental/business/concretes/usersManagers/UsersManager.java
@@ -13,27 +13,33 @@ import ashina.carrental.entities.concretes.users.Users;
 @Service
 public class UsersManager implements UsersService {
 
+    // This class implements the UsersService interface and serves as the business
+    // logic layer for user operations
+
     @Autowired
-    private UsersDao usersDao;
+    private UsersDao usersDao; // Injects an instance of the UsersDao interface for data access
 
     @Override
     public List<Users> getAllUsers() {
-        return usersDao.findAll();
+        return usersDao.findAll(); // Retrieves all users by calling the findAll() method of the UsersDao interface
     }
 
     @Override
     public Optional<Users> findById(int id) {
-        return usersDao.findById(id);
+        return usersDao.findById(id); // Finds a user by their ID by calling the findById() method of the UsersDao
+                                      // interface
     }
 
     @Override
     public Optional<Users> findByEmail(String email) {
-        return usersDao.findByEmail(email);
+        return usersDao.findByEmail(email); // Finds a user by their email address by calling the findByEmail() method
+                                            // of the UsersDao interface
     }
 
     @Override
     public boolean existsByEmail(String email) {
-        return usersDao.existsByEmail(email);
+        return usersDao.existsByEmail(email); // Checks if a user with the specified email address exists by calling the
+                                              // existsByEmail() method of the UsersDao interface
     }
 
 }
