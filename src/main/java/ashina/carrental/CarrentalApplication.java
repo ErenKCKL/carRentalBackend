e16813aac06c6f74b2c43ab3a522cf261c25774e
Eren <eren092003@gmail.com>
2023-06-19 15:46:37 +0200

    first project

---
+++ src/main/java/ashina/carrental/CarrentalApplication.java
@@ -0,0 +1,13 @@
+package ashina.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentalApplication.class, args);
+	}
+
+}
